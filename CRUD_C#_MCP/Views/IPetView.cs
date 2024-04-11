7b795372157fe962923f59f600b8f948c748e737
GusstavoMD <gustavotlx03@gmail.com>
2024-04-11 01:35:12 -0600

    AGREGRAR VIEWS IPET

---
+++ CRUD_C#_MCP/Views/IPetView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_C__MCP.Views
+{
+    public interface IPetView
+    {
+        //Properties - Fields
+        String PetId { get; set; }
+        String PetName { get; set; }
+        String PetType { get; set; }
+        String PetColour { get; set; }
+        String SearchValue { get; set; }
+        String IsEdit { get; set; }
+        String IsSuccessfull { get; set; }
+        String Message { get; set; }
+        // Events
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+        // Methods
+        void SetPetListBindingSource(BindingSource petList);
+        void Show(); // Optional
+    }
+}
